1cae95da4e591bc9afa213000639392f8a0a1b02
dyplm123 <dyplm123@hotmail.com>
2018-09-19 17:58:41 +0800

    seek

---
+++ Build_link.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -s ../3/ename.c.inc ename.c.inc
+ln -s ../3/error_functions.c error_functions.c
+ln -s ../3/error_functions.h error_functions.h
+ln -s ../3/get_num.c get_num.c
+ln -s ../3/get_num.h get_num.h
+ln -s ../3/tlpi_hdr.h tlpi_hdr.h
