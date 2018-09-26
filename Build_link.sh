8a64ac73c89f8d4b2df6a28a6f877f0082eef40f
dyplm123 <dyplm123@hotmail.com>
2018-09-26 17:40:36 +0800

    5

--- Build_link.sh
+++ Build_link.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ln -s ../3/ename.c.inc ename.c.inc
-ln -s ../3/error_functions.c error_functions.c
-ln -s ../3/error_functions.h error_functions.h
-ln -s ../3/get_num.c get_num.c
-ln -s ../3/get_num.h get_num.h
-ln -s ../3/tlpi_hdr.h tlpi_hdr.h
+ln -s ../3/ename.c.inc ./$1/ename.c.inc
+ln -s ../3/error_functions.c ./$1/error_functions.c
+ln -s ../3/error_functions.h ./$1/error_functions.h
+ln -s ../3/get_num.c ./$1/get_num.c
+ln -s ../3/get_num.h ./$1/get_num.h
+ln -s ../3/tlpi_hdr.h ./$1/tlpi_hdr.h
